aca5f4576f0ae8d7249d62adb2f82ea9d0d89f22
Dor-Ron <dor.rondel@gmail.com>
2015-07-06 08:08:15 -0400

    Update git_committer.py
    got rid of unnecessary array

--- util/git_committer/git_committer.py
+++ util/git_committer/git_committer.py
@@ -6,13 +6,9 @@ from os import system
 def committer():
     '''Adds and commits filename(s) entered in argv[1:] to git repo'''
     message = raw_input("Enter commit message: ")
-    arg_array = []
-    for argument in argv[1:]:
-        arg_array.append(argument)
-    for file in arg_array:
+    for file in argv[1:]:
         system("git add %s" % file)
     system("git commit -m '%s'" % message)
 
 if __name__ == "__main__":
     committer()
-
