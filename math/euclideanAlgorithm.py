a683476a6cdabc4effede85af1fb8606b40c283b
Dor-Ron <dor.rondel@gmail.com>
2015-07-06 08:39:18 -0400

    Update euclideanAlgorithm.py
    got rid of unnecessary if statements, made more pep8ish, added docstring

--- math/euclideanAlgorithm.py
+++ math/euclideanAlgorithm.py
@@ -1,32 +1,25 @@
-def euclidean(a, b):
-    if a < b:
-        divisor = a
-        dividend = b
-    elif b < a:
-        divisor = b
-        dividend = a
-    else:
-        print "There's no point in doing this."
+#!/usr/bin/env python 
 
-#now that we know which is the divisor and which is the dividend we can
-#begin dividing and analyzing them.
+def euclidean(a, b):
+    '''finds gcd by means of the Euclidean Algorithm'''
+    divisor = min(a, b)
+    dividend = max(a, b)
+    quotient = dividend / divisor
+    remainder = dividend % divisor
+    
     while dividend % divisor != 0:
-        quotient = dividend / divisor
-        remainder = dividend % divisor
         if remainder == 0:
             gcd = quotient
             print "Your greatest common divisor is %d" % (gcd)
-            break;
+            break
         elif remainder != 0:
             new_dividend = divisor
             new_divisor = dividend
             new_remainder = remainder
             dividend = divisor
             divisor = remainder
-            remainder = divisor/remainder
+            remainder = divisor / remainder
             print "your greatest common divisor is %d" % (new_remainder)
-        else:
-            pass
-
+        
 #test case
-print euclidean(110,20)
+print euclidean(110, 20)
